24f4b6ee0779028fcb6587926dcec63b1e9073ed
uchqun007 <uchqunjonturdiev@gmail.com>
2023-12-02 21:55:23 +0500

    nprogress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import i18n from '@/i18n';
 import '@/styles/globals.css';
+// import 'styles/nprogress';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Router } from 'next/router';
+import { Router, useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { Client, HydrationProvider } from 'react-hydration-provider';
 import { I18nextProvider } from 'react-i18next';
@@ -9,20 +10,21 @@ import NProgress from 'nprogress';
 
 NProgress.configure({ showSpinner: false });
 export default function App({ Component, pageProps }) {
+	const router = useRouter();
+
 	useEffect(() => {
-		const handleRouteStart = () => NProgress.start();
-		const handleRouteDone = () => NProgress.done();
+		const startLoading = () => NProgress.start();
+		const endLoading = () => NProgress.done();
 
-		Router.events.on('routeChangeStart', handleRouteStart);
-		Router.events.on('routeChangeComplete', handleRouteDone);
-		Router.events.on('routeChangeError', handleRouteDone);
-
+		router.events.on('routeChangeStart', startLoading);
+		router.events.on('routeChangeComplete', endLoading);
+		router.events.on('routeChangeError', endLoading);
 		return () => {
-			Router.events.off('routeChangeStart', handleRouteStart);
-			Router.events.off('routeChangeComplete', handleRouteDone);
-			Router.events.off('routeChangeError', handleRouteDone);
+			router.events.off('routeChangeStart', startLoading);
+			router.events.off('routeChangeComplete', endLoading);
+			router.events.off('routeChangeError', endLoading);
 		};
-	}, []);
+	}, [router]);
 	return (
 		<HydrationProvider>
 			<I18nextProvider i18n={i18n}>
